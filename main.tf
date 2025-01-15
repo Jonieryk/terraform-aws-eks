89068ad51b65502bbdf3b7a6a65688347b43d226
Jonieryk <jonatan.joni1@gmail.com>
2025-01-15 21:13:10 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -5,13 +5,17 @@ module "eks_al2" {
   cluster_name    = "${local.name}-al2"
   cluster_version = "1.31"
 
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
   # EKS Addons
   cluster_addons = {
-    coredns                = {}
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
   }
+
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
