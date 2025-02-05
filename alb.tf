d3a2df3f8f1aeb41ede0fa2da65319bdf1058679
Jonieryk <jonatan.joni1@gmail.com>
2025-02-05 20:18:17 +0100

    Alb configuration

---
+++ alb.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "aws_lb_controller" {
+  name = "aws-load-balancer-controller"
+  namespace = kube-system
+  repository = "https://aws.github.io/eks-charts"
+  chart = "aws-load-balancer-controller"
+  
+}
