3428b055701de3bf343d5be71344a945e2098b8f
Jonieryk <jonatan.joni1@gmail.com>
2025-02-05 20:59:09 +0100

    Added cluster load balancer

--- alb.tf
+++ alb.tf
@@ -3,5 +3,16 @@ resource "helm_release" "aws_lb_controller" {
   namespace  = "kube-system"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
+  values = [yamlencode({
+    clusterName = module.eks_al2.cluster_name
+    serviceAccount = {
+        create = true
+        name = "aws-load-balancer-controller"
+        annotations = {
+            "eks.amazonaws.com/role-arn" = module.iam_eks_role.iam_role_arn
+        }
+    }
+  })]
+  
 
 }
