06f27dc89acc25f62024c4ed5caaab2cefde4751
Jonieryk <jonatan.joni1@gmail.com>
2025-02-05 20:27:17 +0100

    ""

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "aws_lb_controller" {
   name       = "aws-load-balancer-controller"
-  namespace  = kube-system
+  namespace  = "kube-system"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
 
