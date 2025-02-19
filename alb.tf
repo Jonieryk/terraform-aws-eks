ad44c0373d63fac0a82763d14cbd3b6873edcdfc
Jonieryk <jonatan.joni1@gmail.com>
2025-02-19 20:08:36 +0100

    Added load balancer role

--- alb.tf
+++ alb.tf
@@ -9,10 +9,20 @@ resource "helm_release" "aws_lb_controller" {
       create = true
       name   = "aws-load-balancer-controller"
       annotations = {
-        "eks.amazonaws.com/role-arn" = module.iam_eks_role.iam_role_arn
+        "eks.amazonaws.com/role-arn" = module.lb_controller_role.iam_role_arn
       }
     }
   })]
+}
 
-
+module "lb_controller_role" {
+  source                                 = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name                              = "aws-load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks_al2.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
 }
