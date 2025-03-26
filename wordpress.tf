a981558a6ae76f5145f9b3b7f1aa00e498556169
Jonieryk <jonatan.joni1@gmail.com>
2025-03-26 20:33:46 +0100

    CHanged version and uncommented values

--- wordpress.tf
+++ wordpress.tf
@@ -3,26 +3,26 @@ resource "helm_release" "wordpress" {
   namespace  = "demo"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "wordpress"
-  version    = "24.1.18"
+  version    = "23.1.29"
 
-# values = [<<EOF
-#     service:
-#       type: LoadBalancer
-#     ingress:
-#       enabled: true
-#       ingressClassName: "alb"
-#       hostname: "wordpress.example.com"
-#       annotations:
-#         kubernetes.io/ingress.class: "alb"
-#         cert-manager.io/cluster-issuer: "letsencrypt-prod"
-#         alb.ingress.kubernetes.io/scheme: internet-facing
-#         alb.ingress.kubernetes.io/target-type: ip
-#         alb.ingress.kubernetes.io/group.name: wordpress
-#         alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'  # Use HTTP instead of HTTPS
-#       tls: 
-#         - hosts: 
-#             - wordpress.szkolenie-devops.com
-#         secretName: wordpress-tls
-#     EOF
-#   ]
+values = [<<EOF
+    service:
+      type: LoadBalancer
+    ingress:
+      enabled: true
+      ingressClassName: "alb"
+      hostname: "wordpress.example.com"
+      annotations:
+        kubernetes.io/ingress.class: "alb"
+        cert-manager.io/cluster-issuer: "letsencrypt-prod"
+        alb.ingress.kubernetes.io/scheme: internet-facing
+        alb.ingress.kubernetes.io/target-type: ip
+        alb.ingress.kubernetes.io/group.name: wordpress
+        alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'  # Use HTTP instead of HTTPS
+      tls: 
+        - hosts: 
+            - wordpress.szkolenie-devops.com
+        secretName: wordpress-tls
+    EOF
+  ]
 }
