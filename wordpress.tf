bf591c94fa28b56cacece3b9afb66169721d3c6c
Jonieryk <jonatan.joni1@gmail.com>
2025-03-26 20:25:00 +0100

    Commendted out values

--- wordpress.tf
+++ wordpress.tf
@@ -5,24 +5,24 @@ resource "helm_release" "wordpress" {
   chart      = "wordpress"
   version    = "24.1.18"
 
-values = [<<EOF
-    service:
-      type: LoadBalancer
-    ingress:
-      enabled: true
-      ingressClassName: "alb"
-      hostname: "wordpress.example.com"
-      annotations:
-        kubernetes.io/ingress.class: "alb"
-        cert-manager.io/cluster-issuer: "letsencrypt-prod"
-        alb.ingress.kubernetes.io/scheme: internet-facing
-        alb.ingress.kubernetes.io/target-type: ip
-        alb.ingress.kubernetes.io/group.name: wordpress
-        alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'  # Use HTTP instead of HTTPS
-      tls: 
-        - hosts: 
-            - wordpress.szkolenie-devops.com
-        secretName: wordpress-tls
-    EOF
-  ]
+# values = [<<EOF
+#     service:
+#       type: LoadBalancer
+#     ingress:
+#       enabled: true
+#       ingressClassName: "alb"
+#       hostname: "wordpress.example.com"
+#       annotations:
+#         kubernetes.io/ingress.class: "alb"
+#         cert-manager.io/cluster-issuer: "letsencrypt-prod"
+#         alb.ingress.kubernetes.io/scheme: internet-facing
+#         alb.ingress.kubernetes.io/target-type: ip
+#         alb.ingress.kubernetes.io/group.name: wordpress
+#         alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'  # Use HTTP instead of HTTPS
+#       tls: 
+#         - hosts: 
+#             - wordpress.szkolenie-devops.com
+#         secretName: wordpress-tls
+#     EOF
+#   ]
 }
